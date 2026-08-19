6fcbee67e3330f5994c4b5dcb06ea8b2b1058a47
vedantgrd <vedantgarud05@gmail.com>
2026-08-19 16:53:10 +0530

    Fix database initialization

--- init_db.py
+++ init_db.py
@@ -1,4 +1,11 @@
+from app import create_app
+from models import db
 from seed import seed
 
-if __name__ == "__main__":
-    seed()
+app = create_app()
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+seed()
